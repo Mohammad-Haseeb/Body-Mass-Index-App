cb7e017817ca6f4cd9baea6c4b7ab2057b138c59
Muhammad Haseeb Hussain <64950545+Mohammad-Haseeb@users.noreply.github.com>
2021-04-25 01:03:13 +0500

    Update card.dart

--- lib/card.dart
+++ lib/card.dart
@@ -10,7 +10,11 @@ class Resuable_Cards extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(10.0),
-      child: ,
+      child: Column(
+        children: [
+
+        ],
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Color(0xFF1D1F33),
